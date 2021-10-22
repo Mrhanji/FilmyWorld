3baa14aa8a75a80de75922a1a74285e03207a3de
Sagar Sharma <sagarsharma995899@gmail.com>
2021-10-22 21:42:52 +0530

    v1

--- lib/Screens/MoviesView.dart
+++ lib/Screens/MoviesView.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flimyworld/Screens/HomeScreen.dart';
 import 'package:flimyworld/api/api_info.dart';
+import 'package:flimyworld/widgets/SimilarMovieView.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -39,10 +40,10 @@ class _MoviesViewState extends State<MoviesView> {
         tagline = tempdata['tagline'];
         ratting = tempdata['vote_average'];
         overview = tempdata['overview'];
+       
         var s = ratting.toInt();
         // rat = s.toDouble();
         rat = double.tryParse('0.' + s.toString());
-      
       });
     });
   }
@@ -54,113 +55,120 @@ class _MoviesViewState extends State<MoviesView> {
         debugShowCheckedModeBanner: false,
         home: Scaffold(
           backgroundColor: Colors.black,
-         
           body: img != null
-              ? Stack(
+              ? SingleChildScrollView(
+                child: Column(
                   children: [
-                    Positioned(
-                        top: 1,
-                        child: Container(
-                            child: Image.network(urls + img.toString()))),
-                    Positioned(
-                        top: 1,
-                        child: Container(
-                          height: size.height * 0.34,
-                          width: size.width,
-                          color: Colors.white12,
-                        )),
-                    Positioned(
-                        top: size.height * 0.05,
-                        left: 1,
-                        child: Container(
-                          width: size.width,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              IconButton(
-                                  icon: Icon(
-                                    CupertinoIcons.left_chevron,
-                                    color: Colors.white,
-                                  ),
-                                  onPressed: () => Navigator.pop(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => HomeScreen()))),
-                              IconButton(
-                                  icon: Icon(
-                                    CupertinoIcons.heart,
-                                    color: Colors.white,
-                                  ),
-                                  onPressed: () => Navigator.pop(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => HomeScreen()))),
-                            ],
-                          ),
-                        )),
-                    Positioned(
-                        top: size.height * 0.2,
-                        left: 15,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
+                    Container(width: size.width,height: size.height,
+                      child: Stack(
                           children: [
-                            Hero(tag: widget.mid,
-                              child: Container(
-                                height: size.height * 0.3,
-                                width: size.width * 0.4,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(20),
-                                    image: DecorationImage(
-                                        image: NetworkImage(
-                                            urls + poster.toString()))),
-                              ),
-                            ),
-                            Container(
-                              width: size.width,
-                              padding: EdgeInsets.only(
-                                  top: size.height * 0.085, left: 20),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Container( width: size.width * 0.5,
-                                    child: Text(name.toString(),
-                                        style: GoogleFonts.actor(
+                            Positioned(
+                                top: 1,
+                                child: Container(
+                                    child: Image.network(urls + img.toString()))),
+                            Positioned(
+                                top: 1,
+                                child: Container(
+                                  height: size.height * 0.34,
+                                  width: size.width,
+                                  color: Colors.white12,
+                                )),
+                            Positioned(
+                                top: size.height * 0.05,
+                                left: 1,
+                                child: Container(
+                                  width: size.width,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      IconButton(
+                                          icon: Icon(
+                                            CupertinoIcons.left_chevron,
                                             color: Colors.white,
-                                            fontSize: size.height * 0.03),maxLines: 2,overflow: TextOverflow.ellipsis,),
+                                          ),
+                                          onPressed: () => Navigator.pop(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) => HomeScreen()))),
+                                      IconButton(
+                                          icon: Icon(
+                                            CupertinoIcons.heart,
+                                            color: Colors.white,
+                                          ),
+                                          onPressed: () => Navigator.pop(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) => HomeScreen()))),
+                                    ],
                                   ),
-                                  Container(
-                                    width: size.width * 0.6,
-                                    child: Text(tagline.toString(),
-                                        style: GoogleFonts.actor(
-                                            color: Colors.grey,
-                                            fontSize: size.height * 0.018),
-                                        maxLines: 1,
-                                        overflow: TextOverflow.ellipsis),
-                                  ),
-                                ],
-                              ),
-                            )
-                          ],
-                        )),
-                    Positioned(
-                        top: size.height * 0.29,
-                        right: 5,
-                        //left: size.width * 0.5,
-                        child: CircularPercentIndicator(
-                          radius: 60.0,
-                          animation: true,
-                          lineWidth: 5.0,
-                          animationDuration: 1500,
-                          percent: rat,
-                          center: new Text(ratting.toString(),
-                              style: TextStyle(color: Colors.white)),
-                          progressColor: Colors.amber,
-                        )),
-
-
-
-Positioned(top:size.height*0.5,
-  child: Row(
+                                )),
+                            Positioned(
+                                top: size.height * 0.2,
+                                left: 15,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Hero(
+                                      tag: widget.mid,
+                                      child: Container(
+                                        height: size.height * 0.3,
+                                        width: size.width * 0.4,
+                                        decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.circular(20),
+                                            image: DecorationImage(
+                                                image: NetworkImage(
+                                                    urls + poster.toString()))),
+                                      ),
+                                    ),
+                                    Container(
+                                      width: size.width,
+                                      padding: EdgeInsets.only(
+                                          top: size.height * 0.085, left: 20),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Container(
+                                            width: size.width * 0.5,
+                                            child: Text(
+                                              name.toString(),
+                                              style: GoogleFonts.actor(
+                                                  color: Colors.white,
+                                                  fontSize: size.height * 0.03),
+                                              maxLines: 2,
+                                              overflow: TextOverflow.ellipsis,
+                                            ),
+                                          ),
+                                          Container(
+                                            width: size.width * 0.6,
+                                            child: Text(tagline.toString(),
+                                                style: GoogleFonts.actor(
+                                                    color: Colors.grey,
+                                                    fontSize: size.height * 0.018),
+                                                maxLines: 1,
+                                                overflow: TextOverflow.ellipsis),
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                )),
+                            Positioned(
+                                top: size.height * 0.29,
+                                right: 5,
+                                //left: size.width * 0.5,
+                                child: CircularPercentIndicator(
+                                  radius: 60.0,
+                                  animation: true,
+                                  lineWidth: 5.0,
+                                  animationDuration: 1500,
+                                  percent: rat,
+                                  center: new Text(ratting.toString(),
+                                      style: TextStyle(color: Colors.white)),
+                                  progressColor: Colors.amber,
+                                )),
+                            Positioned(
+                              top: size.height * 0.5,
+                              child: Row(
                                 children: [
                                   Padding(
                                     padding: const EdgeInsets.only(
@@ -173,19 +181,32 @@ Positioned(top:size.height*0.5,
                                     ),
                                   ),
                                 ],
-                              ),),
-
-Positioned(top: size.height*0.55,
-  child: Container(width: size.width,
-  padding:EdgeInsets.only(left:10,right: 10),
-  
-    child: Text(overview.toString(),style:TextStyle(color: Colors.grey))))
-
-
-
-
+                              ),
+                            ),
+                            Positioned(
+                                top: size.height * 0.55,
+                                child: Column(
+                                  children: [
+                                    Container(
+                                        width: size.width,
+                                        padding: EdgeInsets.only(left: 10, right: 10),
+                                        child: Text(overview.toString(),
+                                            style: TextStyle(color: Colors.grey))),
+                                    Container(
+                                      height: size.height * 0.45,
+                                      width: size.width,
+                                      child: SimilarMovieView(
+                                        mid: widget.mid,
+                                      ),
+                                    )
+                                  ],
+                                )),
+                          ],
+                        ),
+                    ),
                   ],
-                )
+                ),
+              )
               : Lottie.asset('assets/animations/loading.json'),
         ));
   }
---
+++ lib/widgets/SimilarMovieView.dart
@@ -0,0 +1,100 @@
+import 'dart:convert';
+
+import 'package:flimyworld/api/api_info.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:lottie/lottie.dart';
+
+class SimilarMovieView extends StatefulWidget {
+  var mid;
+  SimilarMovieView({Key key, @required this.mid}) : super(key: key);
+
+  @override
+  _SimilarMovieViewState createState() => _SimilarMovieViewState();
+}
+
+class _SimilarMovieViewState extends State<SimilarMovieView> {
+  List poster;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    var url = Uri.parse("https://api.themoviedb.org/3/movie/" +
+        widget.mid.toString() +
+        "/similar?api_key=" +
+        api +
+        "&language=en-US");
+
+    http.get(url).then((value) {
+      setState(() {
+        Map tempdata = jsonDecode(value.body);
+        poster = tempdata['results'];
+        print(url);
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return ListView(
+        scrollDirection: Axis.horizontal,
+        children: poster != null
+            ? poster.map((e) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    child: Column(
+                      children: [
+                        Container(
+                          height: size.height * 0.35,
+                          width: size.width * 0.45,
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      urls + e['poster_path'].toString()),
+                                  fit: BoxFit.fill),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(18.0),
+                              ),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.white12,
+                                  spreadRadius: 2,
+                                  blurRadius: 5,
+                                ),
+                              ]),
+                        ),
+                        Text(
+                          ' ' + e['original_title'].toString(),
+                          style: TextStyle(
+                              fontSize: size.height * 0.025,
+                              color: Colors.white),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.star_rounded,
+                              color: Colors.amber.shade600,
+                            ),
+                            Text('  ' + e['vote_average'].toString(),
+                                style: TextStyle(
+                                    fontSize: size.height * 0.023,
+                                    color: Colors.white))
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }).toList()
+            : [
+                Center(
+                  child: Lottie.asset('assets/animations/loading.json'),
+                )
+              ]);
+  }
+}
